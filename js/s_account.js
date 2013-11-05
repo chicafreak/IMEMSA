77867e1ba387d5229bbe32d7de64065ba9b5b29c
Karina Cruz <karinacruz@icloud.com>
2013-11-05 04:56:54 -0600

    primer archivo imemsa2014

---
+++ js/s_account.js
@@ -0,0 +1,2 @@
+var s_account = 'yamahausamotor,yamahausamotorstage';
+var s_linkInternalFilters = 'javascript:,localhost,yamaha-motor.com,starmotorcycles.com,arinet.com';
---
+++ js/scroll.js
@@ -0,0 +1,9 @@
+   
+function scrollToTop(){
+  scrollBy(0,-3500) ;
+}
+function scrollToTop(){
+for (var i=0;i<3500;i++){
+    scrollBy(0,-200) ;
+  }
+}
